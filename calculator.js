a7fb796f36159c5c86745d2a7f5e43627d89721a
11aciek <10337645+11aciek@users.noreply.github.com>
2024-12-08 12:45:18 +0100

    Add backspace, +/- and .

--- calculator.js
+++ calculator.js
@@ -3,6 +3,10 @@ const display = document.querySelector(".display");
 const operators = Array.from(document.querySelectorAll(".operator"));
 const equals = document.querySelector("#equals");
 const clearButton = document.querySelector("#C");
+const dotButton = document.querySelector("#dot");
+const bckButton = document.querySelector("#BCK");
+const signButton = document.querySelector("#sign");
+
 let number = "0";
 let memory;
 let operator = "";
@@ -42,17 +46,21 @@ function operate(a,b,op) {
 }
 
 function  refreshDisplay(content) {
+  content = content.toString();
   if (content == Infinity) {
 	content = "Oh NO!!";
-  } else if (Number(content) > 999999999999) {
+  } else if (Number(content) > 999999999999 || Number(content) < -99999999999) {
 	content = "No memory!!";
 	clear();
+  } else if (content.length > 12) {
+	content = content.split("").splice(0,12).join("");
   }
 
 	display.textContent = content;
 }
 
 numbers.forEach(btn => btn.addEventListener("click", function() {
+  number = number.toString();
   if (equalsOn || number == "0") {
 	number = btn.id;
 	equalsOn = false;
@@ -61,6 +69,16 @@ numbers.forEach(btn => btn.addEventListener("click", function() {
   }
   refreshDisplay(number);
 }));
+
+
+dotButton.addEventListener("click", function() {
+  if (number && !number.includes(".")) {
+	number += ".";
+  } else if (!number) {
+	number = "0.";
+  }
+  refreshDisplay(number);
+});
 
 operators.forEach(btn => btn.addEventListener("click", function() {
   if (memory) {
@@ -89,4 +107,19 @@ equals.addEventListener("click", function() {
 clearButton.addEventListener("click", function() {
   clear();
   refreshDisplay(0);
+});
+
+bckButton.addEventListener("click", function() {
+  number = number.toString();
+  if (number.length == 1) {
+	number = 0;
+  } else {
+	number = number.toString().slice(0,-1);
+  }
+  refreshDisplay(number);
+});
+
+signButton.addEventListener("click", function() {
+  number = -Number(number);
+  refreshDisplay(number);
 });
